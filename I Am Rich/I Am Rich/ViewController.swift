65556fe219d4863c03e191f9826caa64b7ecbef8
Dahae Kim <iluvdadong@naver.com>
2020-03-23 20:14:35 +0900

    first commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by DaHae Kim on 2020/03/23.
+//  Copyright © 2020 DaHae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
